6a5cfcff7a7cfbe514b457ba97e475b2a29dd20c
wesleytyber <wesleytyber@gmail.com>
2021-10-05 18:32:01 -0300

    udpate projects

--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -1,96 +1,86 @@
-// import { GetStaticPaths, GetStaticProps } from "next";
-// import React from "react";
-// import { Header, LoadingScreen } from "../../../components";
-// import BannerProject from "../../../components/BannerProject";
-// import { getPrismicClient } from "../../../services/prismic";
-// import Prismic from "@prismicio/client";
-// import { useRouter } from "next/router";
-// import * as S from "../../../GlobalStyles/slugStyles";
-
-// interface IProject {
-//   slug: string;
-//   title: string;
-//   type: string;
-//   descriptions: string;
-//   link: string;
-//   thumbnail: string;
-// }
-// interface IProjectProps {
-//   project: IProject;
-// }
-
-// const project = ({ project }: IProjectProps) => {
-//   const router = useRouter();
-
-//   if (router.isFallback) {
-//     return <LoadingScreen />;
-//   }
-
-//   return (
-//     <S.ProjectContainer>
-//       <Header />
-//       <BannerProject
-//         title={project.title}
-//         type={project.type}
-//         imgUrl={project.thumbnail}
-//       />
-//       <main>
-//         <p>{project.descriptions}</p>
-//         <button type="button">
-//           <a href={project.link}>See project online</a>
-//         </button>
-//       </main>
-//     </S.ProjectContainer>
-//   );
-// };
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const project = await prismic.query([
-//     Prismic.Predicates.at("document.type", "project"),
-//   ]);
-
-//   const paths = project.results.map((project) => ({
-//     params: {
-//       slug: project.uid,
-//     },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// };
-
-// export const getStaticProps: GetStaticProps = async (context) => {
-//   const prismic = getPrismicClient();
-//   const { slug }: any = context.params;
-
-//   const response = await prismic.getByUID("project", String(slug), {});
-
-//   const project = {
-//     slug: response.uid,
-//     title: response.data.title,
-//     type: response.data.type,
-//     descriptions: response.data.descriptions,
-//     thumbnail: response.data.thumbnail.url,
-//   };
-
-//   return {
-//     props: {
-//       project,
-//     },
-//     revalidate: 86400,
-//   };
-// };
-// export default project;
-
+import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+import { Header, LoadingScreen } from "../../../components";
+import BannerProject from "../../../components/BannerProject";
+import { getPrismicClient } from "../../../services/prismic";
+import Prismic from "@prismicio/client";
+import { useRouter } from "next/router";
+import * as S from "../../../GlobalStyles/slugStyles";
 
-// import { Container } from './styles';
+interface IProject {
+  slug: string;
+  title: string;
+  type: string;
+  descriptions: string;
+  link: string;
+  thumbnail: string;
+}
+interface IProjectProps {
+  project: IProject;
+}
 
-const Slug: React.FC = () => {
-  return <div />;
+const project = ({ project }: IProjectProps) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <S.ProjectContainer>
+      <Header />
+      <BannerProject
+        title={project.title}
+        type={project.type}
+        imgUrl={project.thumbnail}
+      />
+      <main>
+        <p>{project.descriptions}</p>
+        <button type="button">
+          <a href={project.link}>See project online</a>
+        </button>
+      </main>
+    </S.ProjectContainer>
+  );
 };
 
-export default Slug;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const project = await prismic.query([
+    Prismic.Predicates.at("document.type", "project"),
+  ]);
+
+  const paths = project.results.map((project) => ({
+    params: {
+      slug: project.uid,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const prismic = getPrismicClient();
+  const { slug }: any = context.params;
+
+  const response = await prismic.getByUID("project", String(slug), {});
+
+  const project = {
+    slug: response.uid,
+    title: response.data.title,
+    type: response.data.type,
+    descriptions: response.data.descriptions,
+    thumbnail: response.data.thumbnail.url,
+  };
+
+  return {
+    props: {
+      project,
+    },
+    revalidate: 86400,
+  };
+};
+export default project;
